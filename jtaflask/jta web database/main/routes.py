62fe08a63a65cff977f7079bd1d2aa03ea44a144
Andreas Kokkalis <akokkalis@gmail.com>
2022-05-06 13:58:20 +0300

    Disable Button And Add a tooltip For Send Daily Liq double same day.

--- jtaflask/jta web database/main/routes.py
+++ jtaflask/jta web database/main/routes.py
@@ -302,8 +302,8 @@ def daily_liquidation():
 	if current_user.role=='Representative':	
 		#user_daily_liq = DailyLiquidation.query.filter(DailyLiquidation.owner == current_user.id).all()		
 		user_daily_liq = db.session.query(DailyLiquidation.id, DailyLiquidation.total_sales, DailyLiquidation.bank_deposit, DailyLiquidation.visa_transaction, DailyLiquidation.pre_cancels,DailyLiquidation.cancelled_tickets, DailyLiquidation.total_calculated_amount, DailyLiquidation.date_time_actual,DailyLiquidation.date_liquidated, DailyLiquidation.bank_dep_image, DailyLiquidation.jcc_daily_batch_image,DailyLiquidation.canceled_ticket_image, DailyLiquidation.daily_liquidation_balance, DailyLiquidation.remarks,DailyLiquidation.confirm).filter(DailyLiquidation.owner ==current_user.id ).order_by(DailyLiquidation.id.desc())
-		if already:
-			flash(f'{current_user.name} {current_user.surname} you have Already submited a Daily Liquidation for Today {usefull_functions.current_date()}. For Any Help please Call Supervisor', category='info' )
+		# if already:
+		# 	flash(f'{current_user.name} {current_user.surname} you have Already submited a Daily Liquidation for Today {usefull_functions.current_date()}. For Any Help please Call Supervisor', category='info' )
 		
 		
 
